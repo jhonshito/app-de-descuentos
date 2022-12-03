c25257aac2466532e08eac67b043ba070def7e0d
Jhon <caicedojhon957@gmail.com>
2022-12-02 21:33:47 -0500

    nuevo proyecto

---
+++ propina.js
@@ -0,0 +1,12 @@
+
+// const factura = 
+
+function personas(valor) {
+    console.log(valor)
+    
+    valor == undefined ? valor = 0 :  document.getElementById('factura').innerHTML = 24 *  parseInt(valor)
+
+    // document.getElementById('factura').value == undefined ? document.getElementById('factura').innerHTML = 0 : document.getElementById('factura').innerHTML = 24 *  parseInt(valor)
+}
+
+personas()
