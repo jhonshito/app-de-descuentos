0f3446d27814c16d1507a1be6801af8638ef7b15
Jhon <caicedojhon957@gmail.com>
2022-12-04 09:59:10 -0500

    agregue los descuentos

--- propina.js
+++ propina.js
@@ -1,12 +1,126 @@
 
-// const factura = 
+// capture los botones de descuentos
+const botonOne = document.querySelector('.descuentoOne');
+const botonTwo = document.querySelector('.descuentoTwo');
+const botonThree = document.querySelector('.descuentoThree');
+const botonFour = document.querySelector('.descuentoFour');
+const botonFive = document.querySelector('.descuentoFive');
+const botonSix = document.querySelector('.descuentoSix');
 
-function personas(valor) {
-    console.log(valor)
-    
-    valor == undefined ? valor = 0 :  document.getElementById('factura').innerHTML = 24 *  parseInt(valor)
+// capturo los input de la cantidad de personas y el monto total
+const inputPerson = document.getElementById('inputPerson');
+const factura = document.getElementById('factura');
 
-    // document.getElementById('factura').value == undefined ? document.getElementById('factura').innerHTML = 0 : document.getElementById('factura').innerHTML = 24 *  parseInt(valor)
-}
 
-personas()
+const cantidadPropina = document.querySelector('.amount');
+const total = document.querySelector('.total');
+
+const nuevoError = document.querySelector('.nuevoError');
+
+console.log(botonOne, botonTwo, botonThree, botonFour, botonFive, botonSix, inputPerson, factura);
+const valor = 10000;
+
+// descuento del 5%
+botonOne.addEventListener('click', () => {
+
+    if(inputPerson.value == ''){
+       inputPerson.classList.add('error');
+       nuevoError.innerHTML = 'Can not be zero'
+       return
+    }
+
+    let originValor = valor * inputPerson.value;
+    factura.innerHTML = originValor;
+
+    let fracion = 5 / 100;
+
+    let montoPropina = originValor * fracion;
+    cantidadPropina.innerHTML = `$${montoPropina}`;
+
+    let totalFactura = originValor + montoPropina;
+    total.innerHTML = `$${totalFactura}`
+})
+
+// descuento del 10%
+botonTwo.addEventListener('click', () => {
+
+    if(inputPerson.value == ''){
+       inputPerson.classList.add('error');
+       nuevoError.innerHTML = 'Can not be zero'
+       return
+    }
+
+    let originValor = valor * inputPerson.value;
+    factura.innerHTML = originValor;
+
+    let fracion = 10 / 100;
+
+    let montoPropina = originValor * fracion;
+    cantidadPropina.innerHTML = `$${montoPropina}`;
+
+    let totalFactura = originValor + montoPropina;
+    total.innerHTML = `$${totalFactura}`
+})
+
+// descuento del 15%
+botonThree.addEventListener('click', () => {
+
+    if(inputPerson.value == ''){
+       inputPerson.classList.add('error');
+       nuevoError.innerHTML = 'Can not be zero'
+       return
+    }
+
+    let originValor = valor * inputPerson.value;
+    factura.innerHTML = originValor;
+
+    let fracion = 15 / 100;
+
+    let montoPropina = originValor * fracion;
+    cantidadPropina.innerHTML = `$${montoPropina}`;
+
+    let totalFactura = originValor + montoPropina;
+    total.innerHTML = `$${totalFactura}`
+})
+
+// descuento del 25%
+botonFour.addEventListener('click', () => {
+
+    if(inputPerson.value == ''){
+       inputPerson.classList.add('error');
+       nuevoError.innerHTML = 'Can not be zero'
+       return
+    }
+
+    let originValor = valor * inputPerson.value;
+    factura.innerHTML = originValor;
+
+    let fracion = 25 / 100;
+
+    let montoPropina = originValor * fracion;
+    cantidadPropina.innerHTML = `$${montoPropina}`;
+
+    let totalFactura = originValor + montoPropina;
+    total.innerHTML = `$${totalFactura}`
+})
+
+// descuento del 50%
+botonFive.addEventListener('click', () => {
+
+    if(inputPerson.value == ''){
+       inputPerson.classList.add('error');
+       nuevoError.innerHTML = 'Can not be zero'
+       return
+    }
+
+    let originValor = valor * inputPerson.value;
+    factura.innerHTML = originValor;
+
+    let fracion = 50 / 100;
+
+    let montoPropina = originValor * fracion;
+    cantidadPropina.innerHTML = `$${montoPropina}`;
+
+    let totalFactura = originValor + montoPropina;
+    total.innerHTML = `$${totalFactura}`
+})
